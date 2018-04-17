8ba9ced96922dd40c622f8f3ded9fc0ce3d946fb
jei <jei@localhost.localdomain>
2018-04-17 21:49:09 +0100

    Diretorios

--- install_crs.sh
+++ install_crs.sh
@@ -1,11 +1,9 @@
-cd ~
-
-git clone https://github.com/SpiderLabs/owasp-modsecurity-crs.git
-
+git clone https://github.com/SpiderLabs/owasp-modsecurity-crs.git /home/jei/owasp
+rm -rf /etc/httpd/modsecurity-crs/
 sudo mkdir /etc/httpd/modsecurity-crs/
 
-cd owasp-modsecurity-crs
-sudo cp owasp-modsecurity-crs/crs-setup.conf.example /etc/httpd/modsecurity-crs/crs-setup.conf
-sudo cp -r owasp-modsecurity-crs/rules /etc/httpd/modsecurity-crs/
+cd /home/jei/owasp
+sudo cp crs-setup.conf.example /etc/httpd/modsecurity-crs/crs-setup.conf
+sudo cp -r rules /etc/httpd/modsecurity-crs/
 
 sudo vim /etc/httpd/conf.d/modsecurity.conf
